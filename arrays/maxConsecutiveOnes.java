74869d63de08841cc3fc8e7f7bfe118946f035f8
kavyapantrangi <126671211+kavyapantrangi@users.noreply.github.com>
2024-07-15 22:25:39 +0530

    Create maxConsecutiveOnes.java
    https://leetcode.com/problems/max-consecutive-ones/

---
+++ arrays/maxConsecutiveOnes.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int n=nums.length;
+        int count=0;
+        int ans=0;
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i]==1)
+            {
+                count++;
+                ans=Math.max(count,ans);
+            }
+            else 
+            {
+                count=0;
+                
+
+            } 
+        }
+        return ans;
+    }
+}
