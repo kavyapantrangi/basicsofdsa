23316add1e2bb188b87235cfdb40b3a2d7a4c6eb
kavyapantrangi <126671211+kavyapantrangi@users.noreply.github.com>
2024-07-26 09:09:01 +0530

    Update RearrangeElements.java

--- arrays/RearrangeElements.java
+++ arrays/RearrangeElements.java
@@ -27,3 +27,47 @@ it takes another extra space for storing the positive numbers and storing the ne
         
     }
 }
+
+
+
+Another Approach using by storing only answer and we should not require extra memory for storing positive and negative elements.
+
+
+class Solution {
+    public int[] rearrangeArray(int[] nums) {
+       int i=0;
+       int j=1;
+       int[]result=new int[nums.length];
+       int n=nums.length;
+       for(int i1=0;i1<n;i1++)
+       {
+        if(nums[i1]>0)
+        {
+            result[i]=nums[i1];
+            i=i+2;
+        }
+        else
+        {
+            result[j]=nums[i1];
+            j=j+2;
+        }
+       }
+       return result;
+        
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
