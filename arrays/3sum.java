90fd868d863aa128d58534d0272404d9a26184b3
kavyapantrangi <126671211+kavyapantrangi@users.noreply.github.com>
2024-08-05 19:48:21 +0530

    Update 3sum.java

--- arrays/3sum.java
+++ arrays/3sum.java
@@ -61,4 +61,45 @@ list.add(ans);
         return res;
     }
 }
-//Tc(O2)
+//Tc :O(N2 * log(no. of unique triplets)
+sc:O(n)//for set data structure
+By using the  Two pointer approach
+    class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        // List<Integer>ans=new ArrayList<>();
+        List<List<Integer>>list=new ArrayList<>();
+        Arrays.sort(nums);
+        int n=nums.length;
+        for(int i=0;i<n;i++)
+        {
+            if(i>0&&nums[i-1]==nums[i])continue;
+            int j=i+1;
+            int k=n-1;
+            while(j<k)
+            {
+                int sum=nums[i]+nums[j]+nums[k];
+                if(sum<0)
+                {
+                    j++;
+                }
+                else if(sum>0)
+                {
+                    k--;
+                }
+                else
+                {
+                    List<Integer>a=Arrays.asList(nums[i],nums[j],nums[k]);
+                    list.add(a);
+                    j++;
+                    k--;
+                    while(j<k&&nums[j]==nums[j-1])j++;
+                    while(j<k&&nums[k]==nums[k+1])k--;
+
+                }
+            }
+        }
+        return list;
+    }
+}
+//Tc:O(NlogN)+O(N*2)
+Sc:O(1)
