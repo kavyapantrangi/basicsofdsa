1dd86ef792bebe3344b05010ef7f6802aaa1dd9e
kavyapantrangi <126671211+kavyapantrangi@users.noreply.github.com>
2024-08-15 20:10:35 +0530

    Create CeilAndFloor.java

---
+++ BinarySearch/CeilAndFloor.java
@@ -0,0 +1,53 @@
+#########################The Ceil Value:The Value That is Smallest Of Greater VAlues#####################
+
+
+
+class Solution {
+    public int search(int[] nums, int target) {
+        int low=0;
+        int high=nums.length-1;
+        while(low<=high)
+        {
+            int mid=(low+high)/2;
+            if(nums[mid]==target)
+            {
+                return mid;
+            }
+            else if(nums[mid]>target)
+            {
+                high=mid-1;
+            }
+            else
+            {
+                low=mid+1;
+            }
+        }
+        return low;
+        
+    }
+}
+#######################FLoor Value:The value that is the Greater smallest##############
+class Solution {
+    public int search(int[] nums, int target) {
+        int low=0;
+        int high=nums.length-1;
+        while(low<=high)
+        {
+            int mid=(low+high)/2;
+            if(nums[mid]==target)
+            {
+                return mid;
+            }
+            else if(nums[mid]>target)
+            {
+                high=mid-1;
+            }
+            else
+            {
+                low=mid+1;
+            }
+        }
+        return high;
+        
+    }
+}
