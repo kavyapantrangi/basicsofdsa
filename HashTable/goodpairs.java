5185536eeb38f23e177fefae320089f0b6efb25f
kavyapantrangi <126671211+kavyapantrangi@users.noreply.github.com>
2024-08-23 22:33:20 +0530

    Create goodpairs.java

---
+++ HashTable/goodpairs.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int numIdenticalPairs(int[] nums) {
+        Map<Integer,Integer>map=new HashMap<>();
+        int count=0;
+        int n=nums.length;
+        for(int i=0;i<n;i++)
+        {
+            if(map.containsKey(nums[i]))
+            {
+                count+=map.get(nums[i]);
+            }
+            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+        }
+        return count;
+        
+    }
+}
