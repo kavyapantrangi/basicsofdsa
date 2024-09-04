3a385ace776be3934b46b7f5318ea5efb27f02ae
kavyapantrangi <126671211+kavyapantrangi@users.noreply.github.com>
2024-09-04 18:23:05 +0530

    Create Search2D.java

---
+++ BinarySearch/Search2D.java
@@ -0,0 +1,20 @@
+Bruteforce approach:
+  class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        //btuteforce
+        int n=matrix.length;
+        int m=matrix[0].length;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(matrix[i][j]==target)
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+        
+    }
+}
