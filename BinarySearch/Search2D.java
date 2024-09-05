28d3c3ff2646c20645576e905a13e6127ab146ae
kavyapantrangi <126671211+kavyapantrangi@users.noreply.github.com>
2024-09-05 19:25:23 +0530

    Update Search2D.java

--- BinarySearch/Search2D.java
+++ BinarySearch/Search2D.java
@@ -18,3 +18,41 @@ Bruteforce approach:
         
     }
 }
+Bettter Approach:
+  class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int n=matrix.length;
+        for(int i=0;i<n;i++)
+        {
+            if(matrix[i][0]<=target&&target<=matrix[i][n-1])
+            {
+             return  bs(matrix[i],target);
+            }
+        }
+        return false;
+        
+    }
+    public boolean bs(int[]arr,int tar)
+    {
+        int low=0;
+        int high=arr.length-1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(arr[mid]==tar)
+            {
+                return true;
+            }
+            else if(arr[mid]>tar)
+            {
+
+                high=mid-1;
+            }
+            else
+            {
+                low=mid+1;
+            }
+        }
+        return false;
+
+    }
+}
