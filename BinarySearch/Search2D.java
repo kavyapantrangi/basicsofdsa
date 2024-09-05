a286dd7cdf3e7cc8cae8b5790d033bf1973b24ec
kavyapantrangi <126671211+kavyapantrangi@users.noreply.github.com>
2024-09-05 19:36:59 +0530

    Update Search2D.java

--- BinarySearch/Search2D.java
+++ BinarySearch/Search2D.java
@@ -56,3 +56,22 @@ Bettter Approach:
 
     }
 }
+optimal approach:
+  class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int n=matrix.length;
+        int m=matrix[0].length;
+        int high=n*m-1;
+        int low=0;
+        while(low<=high)
+        {
+            int mid=(low+high)/2;
+            int r=mid/m;
+            int c=mid%m;
+            if(matrix[r][c]==target)return true;
+            else if(matrix[r][c]>target)high=mid-1;
+            else low=mid+1;
+        }
+        return false;
+    }
+}
