dee50ad289fa5b474465113fb1423ebe2248f624
kavyapantrangi <126671211+kavyapantrangi@users.noreply.github.com>
2024-10-04 18:07:44 +0530

    Create EvenLinked.java

---
+++ LinkedList/EvenLinked.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isLengthEven(Node head) {
+        int count=0;
+        Node temp=head;
+        while(temp!=null)
+        {
+            count++;
+            temp=temp.next;
+        }
+        if(count%2==0)return  true;
+        return false;
+        // code here
+    }
+}
