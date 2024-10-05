751cacf2de0e53dff8cd407897581eda3bc6e136
kavyapantrangi <126671211+kavyapantrangi@users.noreply.github.com>
2024-10-05 22:48:23 +0530

    Update CycleDetection.java

--- LinkedList/CycleDetection.java
+++ LinkedList/CycleDetection.java
@@ -1,3 +1,24 @@
+BruteForce Approah:
+    public class Solution {
+    public boolean hasCycle(ListNode head) {
+       
+        Map<ListNode,Integer>map=new HashMap<>();
+        ListNode temp=head;
+        while(temp!=null)
+        {
+            if(map.containsKey(temp))return true;
+            else
+            {
+                map.put(temp,1);
+            }
+            temp=temp.next;
+        }
+        return false;
+    }
+}
+
+
+Optimal Approach:
 public class Solution {
     public boolean hasCycle(ListNode head) {
        
