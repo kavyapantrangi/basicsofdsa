42f98c89571c66d048350388242c862025d4cb34
kavyapantrangi <126671211+kavyapantrangi@users.noreply.github.com>
2024-12-28 23:10:20 +0530

    Create validBST.java

---
+++ DFS/validBST.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+       return validate(root,null,null);
+
+
+        
+    }
+    private boolean validate(TreeNode node,Integer lower,Integer upper)
+    {
+        if(node==null)return true;
+        int val=node.val;
+        if(lower!=null&&val<=lower)return false;
+        if(upper!=null&&val>=upper)return false;
+        if(!validate(node.right,val,upper))return false;
+        if(!validate(node.left,lower,val))return false;
+        return true;
+    }
+}
