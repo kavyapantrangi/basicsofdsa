4f25339c715808b37fd3ee9d2a8cddea0afba9d3
kavyapantrangi <126671211+kavyapantrangi@users.noreply.github.com>
2025-01-16 15:05:10 +0530

    Create maxsub0&1.java

---
+++ GFGPOTD/maxsub0&1.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int maxLen(int[] arr) {
+        int max=0;
+        int n=arr.length;
+        Map<Integer,Integer>map=new HashMap<>();
+       int sum=0;
+       map.put(0,-1);
+       for(int i=0;i<n;i++)
+       {
+           sum+=(arr[i]==0)?-1:1;
+           if(map.containsKey(sum))
+           {
+               max=Math.max(max,i-map.get(sum));
+           }
+           else
+           {
+           map.put(sum,i);
+           }
+       }
+       return max;
+    }
+}
