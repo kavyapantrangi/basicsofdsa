72b7d7e9352404e53d0d5947e7dacf9f2450ff2c
kavyapantrangi <126671211+kavyapantrangi@users.noreply.github.com>
2025-01-18 16:22:57 +0530

    added Gfg problem solution

--- Recursion/DP/jumpGame2.java
+++ Recursion/DP/jumpGame2.java
@@ -17,3 +17,34 @@ class Solution {
         
     }
 }
+
+//gfg minmumjumps ::
+
+
+class Solution {
+    static int minJumps(int[] arr) {
+        int n=arr.length;
+        int jumps=0;
+        int curr=0;
+        int farthest=0;
+        if(arr[0]==0)return -1;
+        for(int i=0;i<n;i++)
+        
+        {
+            //if we reached the last index;
+            if(i==n-1)return jumps;
+            farthest=Math.max(farthest,arr[i]+i);
+            //if we reach the edge of current range
+            if(i==curr)
+            {
+                jumps++;
+                curr=farthest;
+            }
+            //if the current range doesnt exist
+            if(curr<=i)return -1;
+        }
+        
+        return -1;
+        // code here
+    }
+}
