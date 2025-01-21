c828e4c78900a54a72dd2ff15ae810b092e616dd
kavyapantrangi <126671211+kavyapantrangi@users.noreply.github.com>
2025-01-21 20:42:32 +0530

    Create ReverseGroup.java

---
+++ GFGPOTD/ReverseGroup.java
@@ -0,0 +1,34 @@
+class Solution {
+    public static Node reverseKGroup(Node head, int k) {
+        // code here
+        if(head==null) return head;
+        if(len(head)<k) return rev(head);
+        Node curr=head,next=null,prev=null;
+        for(int i=0;i<k;i++){
+           next=curr.next;
+           curr.next=prev;
+           prev=curr;
+           curr=next;
+        }
+        head.next=reverseKGroup(curr,k);
+        return prev;
+    }
+    private static int len(Node head){
+        int n=0;
+        while(head!=null){
+            n++;
+            head=head.next;
+        }
+        return n;
+    }
+    private static Node rev(Node head){
+        Node curr=head,next=null,prev=null;
+        while(curr!=null){
+           next=curr.next;
+           curr.next=prev;
+           prev=curr;
+           curr=next;
+        }
+        return prev;
+    }
+}
